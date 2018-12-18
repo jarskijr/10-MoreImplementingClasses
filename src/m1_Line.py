57fc6329692191a6f203ff82e79073288c64f04c
jarskijr <jarskijr@rose-hulman.edu>
2018-12-18 11:41:56 -0500

    Done...ish

--- src/m1_Line.py
+++ src/m1_Line.py
@@ -566,7 +566,7 @@ class Line(object):
           :rtype: Line:
         """
         # ---------------------------------------------------------------------
-        # TODO: 10.
+        # DONE: 10.
         #   a. READ the above specification, including the Example.
         #        ** ASK QUESTIONS AS NEEDED. **
         #        ** Be sure you understand it, ESPECIALLY the Example.
@@ -575,6 +575,10 @@ class Line(object):
         #        They include the Example in the above doc-string.
         # ---------------------------------------------------------------------
 
+        start = Point(self.start.x - other_line.start.x, self.start.y - other_line.start.y)
+        end = Point(self.end.x - other_line.end.x, self.end.y - other_line.end.y)
+        line_minus = Line(start, end)
+        return line_minus
     def midpoint(self):
         """
         What comes in:
@@ -593,7 +597,7 @@ class Line(object):
           :rtype: Point
         """
         # ---------------------------------------------------------------------
-        # TODO: 11.
+        # DONE: 11.
         #   a. READ the above specification, including the Example.
         #        ** ASK QUESTIONS AS NEEDED. **
         #        ** Be sure you understand it, ESPECIALLY the Example.
@@ -602,6 +606,8 @@ class Line(object):
         #        They include the Example in the above doc-string.
         # ---------------------------------------------------------------------
 
+        midpoint = Point((self.end.x + self.start.x)/2, (self.end.y + self.start.y)/2)
+        return midpoint
     def is_parallel(self, line2):
         """
         What comes in:
@@ -629,8 +635,27 @@ class Line(object):
           :type  line2: Line
           :rtype: bool
         """
+        selfslopex = (self.end.x - self.start.x)
+        line2slopex = (line2.end.x - line2.start.x)
+
+        if line2slopex == 0:
+            if line2slopex == selfslopex:
+                return True
+            else:
+                return False
+        if selfslopex == 0:
+            return False
+        selfslope =((self.end.y - self.start.y)/(self.end.x - self.start.x))
+        line2slope = ((line2.end.y - line2.start.y)/ (line2.end.x - line2.start.x))
+        if round(line2slope, 10) == round(selfslope, 10):
+            return True
+        else:
+            return False
+
+
+
         # ---------------------------------------------------------------------
-        # TODO: 12.
+        # DONE: 12.
         #   a. READ the above specification, including the Example.
         #        ** ASK QUESTIONS AS NEEDED. **
         #        ** Be sure you understand it, ESPECIALLY the Example.
