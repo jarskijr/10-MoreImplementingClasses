8fab5e8c5909d6362b1b0dba1cfdda9eec5d6e77
jarskijr <jarskijr@rose-hulman.edu>
2018-12-18 19:57:31 -0500

    Done...ish

--- src/m2_baby_class.py
+++ src/m2_baby_class.py
@@ -3,8 +3,8 @@ A   Baby   class and functions that use/test it.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Jacob Jarski.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -42,7 +42,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 2.
+# DONE: 2.
 #
 #  Step 2a:  Implement a class called   Baby   that has a constructor method
 #            (__init__) and two other methods, as described below.
@@ -98,6 +98,25 @@ def main():
 #         'Baby <your baby's name> is CRYING uncontrollably!  Feed the Baby!'
 #
 ###############################################################################
+class Baby(object):
+    """Is a baby I guess?"""
+    def __init__(self, name):
+        self.name = name
+        self.babyfed = 0
+        print("Hello baby", self.name)
+    def feed_baby(self):
+        print("Thank you for feeding baby",self.name)
+        self.babyfed = 0
+    def hour_passes(self):
+        if self.babyfed == 0 :
+            print("Baby",self.name,"is sleeping.")
+        if self.babyfed == 1 :
+            print("Baby", self.name,"is awake. Time for food.")
+        if self.babyfed >= 2:
+            print("Baby", self.name,"is crying uncontrollably! Feed the Baby!")
+        self.babyfed += 1
+
+
 
 ###############################################################################
 # The   Baby   class (and its methods) should begins here.
